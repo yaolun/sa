f1db719912bb34d38fd5364e299f56610130dc33
Yao-Lun Yang <allenya@gmail.com>
2016-08-08 16:51:20 -0400

    masked out flux=0 (NaN) point in the output

--- pacs_weight.py
+++ pacs_weight.py
@@ -122,33 +122,12 @@ def pacs_weight(cubedir, obj, aper_size, outdir, fits_for_header,
 
         flux = flux + foo['Flux_Density(Jy)'][trimmer]*weight[i-1]
 
-    # # dervie the scaling factor based on the photometry fluxes
-    # phot = ascii.read(photpath,comment='%')
-    # pacs_phot = np.array([phot['flux'][phot['wavelength'] == 70].data,
-    #                       phot['flux'][phot['wavelength'] == 100].data,
-    #                       phot['flux'][phot['wavelength'] == 160].data])
-    # pacs_phot_unc = np.array([phot['error'][phot['wavelength'] == 70].data,
-    #                           phot['error'][phot['wavelength'] == 100].data,
-    #                           phot['error'][phot['wavelength'] == 160].data])
-    # phot = {'flux': pacs_phot, 'uncertainty': pacs_phot_unc}
-    # from scipy.interpolate import interp1d
-    # f = interp1d(wl, flux)
-    # s1, s1_unc = pacs_phot[0]/f(70), pacs_phot_unc[0]/f(70)
-    # s2, s2_unc = pacs_phot[1]/f(100), pacs_phot_unc[1]/f(100)
-    # s3, s3_unc = pacs_phot[2]/f(160), pacs_phot_unc[2]/f(160)
-    # s = np.array([s1,s2,s3])
-    # s_unc = np.array([s1_unc, s2_unc, s3_unc])
-    #
-    # # take a weighted mean and unbiased variance
-    # scaling = np.sum(1/s_unc**2*s)/np.sum(1/s_unc**2)
-    # scaling_unc = ( 1./(len(s)-1) * np.sum(1/s_unc**2*(s - scaling)**2)/np.sum(1/s_unc**2) )**0.5
-    # print 'Scaling: %f +/- %f' % (scaling, scaling_unc)
-
     # write the weighted spectrum into a file
     foo = open(outdir+obj+'_pacs_weighted_'+suffix+'.txt','w')
     foo.write('{} \t {}\n'.format('Wavelength(um)', 'Flux_Density(Jy)'))
     for i in range(len(wl)):
-        foo.write('{} \t {}\n'.format(wl[i], flux[i]))
+        if flux[i] != 0:
+            foo.write('{} \t {}\n'.format(wl[i], flux[i]))
     foo.close()
 
     print 'Weighted spectrum saved at ', outdir+obj+'_pacs_weighted_'+suffix+'.txt'
